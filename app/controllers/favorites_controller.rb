8d5f3a8f18f61e709c177c9a2f88137b2d796df9
kasagata <k.sagata.1207@gmail.com>
2024-02-20 19:08:21 +0900

    メンタリング後

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,26 +1,16 @@
 class FavoritesController < ApplicationController
-  before_action :set_blog
 
   def create
     respond_to do |format|
-      current_user.favorites.create(blog_id: params[:blog_id])
-      format.js { render :create }
+      @favorite = current_user.favorites.create(blog_id: params[:blog_id])
+      format.js { render :favorite }
     end
   end
 
   def destroy
     respond_to do |format|
-      current_user.favorites.find_by(id: params[:id]).destroy
-      format.js { render :destroy }
-    end
-  end
-
-  def set_blog
-    if params[:blog_id].nil?
-      favorite = Favorite.find(params[:id])
-      @blog = Blog.find(favorite.blog_id)
-    else
-      @blog = Blog.find(params[:blog_id])
+      @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+      format.js { render :favorite }
     end
   end
 
